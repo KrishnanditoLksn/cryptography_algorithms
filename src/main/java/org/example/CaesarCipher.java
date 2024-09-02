1e8dd6beeda3bae8ca166e58ed60084df65dac17
Gorthdzx <krislaksana123@gmail.com>
2024-09-02 19:55:53 +0700

    feat: encryption algo

--- src/main/java/org/example/CaesarCipher.java
+++ src/main/java/org/example/CaesarCipher.java
@@ -2,8 +2,22 @@ package org.example;
 
 public class CaesarCipher {
 
-    public String generate_encryption() {
-        return "";
+    public String generate_encryption(String text, int shift) {
+        StringBuilder cipher = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char msg = text.charAt(i);
+            if(Character.isLetter(msg)){
+                int pos = msg;
+                if(Character.isUpperCase(msg)){
+                    pos = (pos + shift) % 26 + 65;
+                    cipher.append((char)pos);
+                } else {
+                    pos = (pos + shift - 97) % 26 + 97;
+                    cipher.append((char)pos);
+                }
+            }
+        }
+        return cipher.toString();
     }
 
     public String generate_decryption() {
