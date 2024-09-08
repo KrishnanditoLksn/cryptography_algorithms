8b2c82bd9011ee2e805e4343d448f2c5a8479f05
Gorthdzx <krislaksana123@gmail.com>
2024-09-08 21:10:06 +0700

    feat: decryption algo

--- src/main/java/org/example/CaesarCipher.java
+++ src/main/java/org/example/CaesarCipher.java
@@ -6,21 +6,40 @@ public class CaesarCipher {
         StringBuilder cipher = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
             char msg = text.charAt(i);
-            if(Character.isLetter(msg)){
+            if (Character.isLetter(msg)) {
                 int pos = msg;
-                if(Character.isUpperCase(msg)){
+                if (Character.isUpperCase(msg)) {
                     pos = (pos + shift) % 26 + 65;
-                    cipher.append((char)pos);
+                    cipher.append((char) pos);
                 } else {
                     pos = (pos + shift - 97) % 26 + 97;
-                    cipher.append((char)pos);
+                    cipher.append((char) pos);
                 }
             }
         }
         return cipher.toString();
     }
 
-    public String generate_decryption() {
-        return "";
+    public String generate_decryption(String text, int shift) {
+        StringBuilder stringBuilderDecrypt = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            int pos = text.charAt(i);
+            if (Character.isLetter(pos)) {
+                pos = (pos - shift - 65) % 26;
+                if (pos < 0) {
+                    pos = 26 + pos;
+                }
+                pos += 65;
+                stringBuilderDecrypt.append((char) pos);
+            } else {
+                pos = (pos - shift - 97) % 26;
+                if (pos < 0) {
+                    pos = 26 + pos;
+                }
+                pos += 97;
+                stringBuilderDecrypt.append((char) pos);
+            }
+        }
+        return stringBuilderDecrypt.toString();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CaesarCipher cipher = new CaesarCipher();
         System.out.println(cipher.generate_encryption("mahasiswa usd", 7));
+        System.out.println(cipher.generate_decryption("awas ada sule " , 3));
     }
 }
